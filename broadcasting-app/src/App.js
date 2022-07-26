f3fb2bed2e449ef00d05cf0ec27d59f849a46ac0
alex-straw <as17163@bristol.ac.uk>
2022-07-26 11:48:06 +0100

    added a landing page functional component

--- broadcasting-app/src/App.js
+++ broadcasting-app/src/App.js
@@ -1,10 +1,10 @@
+import React from "react";
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Default </h1>
-    </div>
+    <LandingPage/>
   );
 }
 
